846d6758dcc7ac5ab08fc9c1ab7ac0017e9ec15e
pinkcoderunicorn <70461093+pinkcoderunicorn@users.noreply.github.com>
2021-08-29 10:41:35 +0530

    Add files via upload

---
+++ 108/108code.py
@@ -0,0 +1,9 @@
+import pandas as pd 
+import csv 
+import plotly.figure_factory as ff
+
+df = pd.read_csv("datafile1.csv")
+fig= ff.create_distplot([df["Height(Inches)"].tolist()],["Height"],show_hist=False)
+fig.show()
+
+
